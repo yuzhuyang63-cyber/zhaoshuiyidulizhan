de8473e6a559bcb9b7bbee17f11b3517ad4be610
elijah <2201679387@qq.com>
2026-07-12 04:53:40 +0100

    Revert "Include nested review pages in frontend deploy"
    This reverts commit 2c08eb59c6f57a7785f72c85e3760cd921ee3a7d.

--- deploy/manage.sh
+++ deploy/manage.sh
@@ -108,10 +108,6 @@ copy_static_files() {
         cd "$PROJECT_DIR"
         sudo mkdir -p "$STATIC_DIR"
         sudo cp ./*.html ./*.css ./*.js robots.txt sitemap.xml "$STATIC_DIR/"
-        if [[ -d reviews ]]; then
-            sudo mkdir -p "$STATIC_DIR/reviews"
-            sudo cp -r reviews/. "$STATIC_DIR/reviews/"
-        fi
         sudo cp -r assets media "$STATIC_DIR/"
     )
 }
